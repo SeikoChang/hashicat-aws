b63fb3a1ccd02d931be0a4c527371eb6575ce559
Seiko Chang <seiko0718@yahoo.com.tw>
2020-06-24 09:01:50 +0000

    update vpc

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source  = "app.terraform.io/seiko/vpc/aws"
-  version = "2.44.0"
-  cidr_block = "10.0.0.0/16"
+  source  = "app.terraform.io/seiko/vpc/aws",
+  version = "2.44.0",
+  cidr_block = "10.0.0.0/16",
 }
